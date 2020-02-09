b59adaa36ea8e3e3c548475735871aca3dccd2ac
Dolphin Oracle <dolphinoracle@gmail.com>
2020-02-09 18:58:43 -0500

    mx19.1 theme changes (mostly)

--- Themes/mx/conky/conky-startup.sh
+++ Themes/mx/conky/conky-startup.sh
@@ -9,5 +9,5 @@ fc-cache -r /usr/share/fonts/extra
 fi
 sleep 20
 killall conky
-cd "/home/$USER/.conky/MX-Cowon"
+cd "/home/$USER/.conky/MX-CowonBlue"
 conky -c "/home/$USER/.conky/MX-CowonBlue/MX-Cowon_blue_roboto" &
