f57c9da75b0f72c5063e5e5cbc023aad205ce18e
Adrian <adrian@mxlinux.org>
2020-06-09 17:05:10 -0400

    add pager

--- Themes/mxkde/theme.sh
+++ Themes/mxkde/theme.sh
@@ -59,6 +59,7 @@ copy_file kglobalshortcutsrc /etc/skel/.config/
 copy_file kscreenlockerrc /etc/skel/.config/ 
 copy_file ksmserverrc /etc/skel/.config/ 
 copy_file kwalletrc /etc/skel/.config/ 
+copy_file kwinrc /etc/skel/.config/ 
 copy_file org.kde.yakuake.desktop /etc/skel/.config/autostart/
 copy_file plasma-org.kde.plasma.desktop-appletsrc /etc/skel/.config/
 copy_file set_wallpaper.desktop /etc/skel/.config/autostart/
