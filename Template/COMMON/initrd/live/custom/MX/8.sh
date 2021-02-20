dc6bbfa9ad1c08e1d78bd32098b6f491bcf40e40
Dolphin Oracle <dolphinoracle@gmail.com>
2021-02-20 14:59:00 -0500

    prevent symlink to non-existant resolv.conf

--- Template/COMMON/initrd/live/custom/MX/8.sh
+++ Template/COMMON/initrd/live/custom/MX/8.sh
@@ -6,7 +6,7 @@ antix_specific_code() {
     mkdir -p $dir/var/log/samba $dir/var/log/fsck
 
     # Create the /etc/resolv.conf symlink if it does not already exist
-    ln -s /etc/resolvconf/run/resolv.conf $dir/etc/resolv.conf 2>/dev/null
+    #ln -s /etc/resolvconf/run/resolv.conf $dir/etc/resolv.conf 2>/dev/null
 
     rm -f $dir/etc/fstab.hotplug
     
