6eaaaf82d97db5076097c51fa19b7bbe941401fc
Dolphin Oracle <dolphinoracle@gmail.com>
2021-03-30 09:53:15 -0400

    Update 8.sh
    reverse bullseye change that snuck into buster

--- Template/COMMON/initrd/live/custom/MX/8.sh
+++ Template/COMMON/initrd/live/custom/MX/8.sh
@@ -6,11 +6,8 @@ antix_specific_code() {
     mkdir -p $dir/var/log/samba $dir/var/log/fsck
 
     # Create the /etc/resolv.conf symlink if it does not already exist
-    #ln -s /etc/resolvconf/run/resolv.conf $dir/etc/resolv.conf 2>/dev/null
+    /etc/resolvconf/run/resolv.conf $dir/etc/resolv.conf 2>/dev/null
     
-    #remove /etc/resolv.conf no matter what it is
-	rm -f $dir/etc/resolv.conf
-
     rm -f $dir/etc/fstab.hotplug
     
     local protect=$dir/etc/live/protect
