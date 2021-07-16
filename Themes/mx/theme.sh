247fb302fdd7bdefa2b01b20ec37e9d2709496f9
Dolphin Oracle <dolphinoracle@gmail.com>
2021-07-16 11:03:07 -0400

    remove sandbox conf file
    no longer needed, bullseye defaults are opposite of busters.

--- Themes/mx/theme.sh
+++ Themes/mx/theme.sh
@@ -19,7 +19,6 @@ copy_file conky.desktop  /etc/skel/.config/autostart/
 copy_file rc.local              /etc/
 copy_file libuser.conf          /etc/
 copy_file modules               /etc/
-copy_file 99-sandbox-mx.conf /etc/sysctl.d/
 copy_file timezone		/etc/
 #copy_file 98vboxadd-xclient     /etc/X11/Xsession.d/
 #copy_file virtualbox-guest-utils	/etc/init.d/
