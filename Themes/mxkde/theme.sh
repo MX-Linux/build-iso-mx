65d70700a2be2421902288eb8a218f3f4439d6b3
Adrian <adrian@mxlinux.org>
2021-07-25 10:03:51 -0400

    Don't copy /etc/skel/Desktop; update default-kde

--- Themes/mxkde/theme.sh
+++ Themes/mxkde/theme.sh
@@ -51,7 +51,7 @@ copy_file plymouthd.conf /etc/plymouth/
 #copy_file luckybackup-su.desktop /usr/share/applications/
 #copy_file luckybackup.svg /usr/share/icons/hicolor/scalable/apps/
 
-copy_dir Desktop/               /etc/skel/Desktop/           --create
+#copy_dir Desktop/               /etc/skel/Desktop/           --create
 #copy_dir cache		/etc/skel/.cache/ 	--create
 
 ##alpha
