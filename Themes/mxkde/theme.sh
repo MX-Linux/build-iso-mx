42e147f72eebbcfb81370b70b5aed3b7e924a7fd
Adrian <adrian@mxlinux.org>
2021-08-21 07:54:01 -0400

    Fix root login in KDE

--- Themes/mxkde/theme.sh
+++ Themes/mxkde/theme.sh
@@ -3,9 +3,9 @@
 THEME_DIR=$(dirname $(readlink -f $0))
 source $THEME_DIR/../theme-functions.sh
 start_theme "$@"
-
 rm -R /etc/skel/.conky
 copy_file grub                  /etc/default/
+copy_file 55-tweak-override.conf /etc/polkit-1/localauthority.conf.d/
 copy_file 10_linux              /etc/grub.d/
 #copy_file 20_memtest86+         /etc/grub.d/
 copy_dir desktop-base/               /usr/share/desktop-base/          --create
