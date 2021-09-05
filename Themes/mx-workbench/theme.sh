39e038d93b16cd1a2d1ff19a1a5180ebf9932ce0
Adrian <adrian@mxlinux.org>
2021-09-05 17:29:29 -0400

    Workbench: update theme

--- Themes/mx-workbench/theme.sh
+++ Themes/mx-workbench/theme.sh
@@ -4,11 +4,13 @@ THEME_DIR=$(dirname $(readlink -f $0))
 source $THEME_DIR/../theme-functions.sh
 start_theme "$@"
 
-#copy_file grub                  /etc/default/
-#copy_file 10_linux              /etc/grub.d/
+rm -R /etc/skel/.conky
+copy_file grub                  /etc/default/
+copy_file 55-tweak-override.conf /etc/polkit-1/localauthority.conf.d/
+copy_file 10_linux              /etc/grub.d/
 #copy_file 20_memtest86+         /etc/grub.d/
 #copy_dir conky/               /etc/skel/.conky/          --create
-# copy_dir extra/               /usr/share/fonts/extra       --create
+#copy_dir extra/               /usr/share/fonts/extra       --create
 copy_file alsamixer.desktop /usr/share/applications/
 #copy_file conky-toggle.desktop /usr/share/applications/
 #copy_file conkytoggle.sh /usr/local/bin/
@@ -16,10 +18,8 @@ copy_file alsamixer.desktop /usr/share/applications/
 copy_file rc.local              /etc/
 copy_file libuser.conf          /etc/
 copy_file modules               /etc/
-copy_file 99-sandbox-mx.conf /etc/sysctl.d/
 copy_file timezone		/etc/
 #copy_file 98vboxadd-xclient     /etc/X11/Xsession.d/
-#copy_file catfish.desktop	/usr/share/applications/ 
 copy_file display-im6.desktop		/usr/share/applications/ 
 copy_file display-im6.q16.desktop	/usr/share/applications/
 #copy_file Window_Buttons.desktop	/usr/share/applications/
@@ -36,13 +36,10 @@ copy_file pppoeconf.desktop     /usr/share/applications/
 #copy_file i915-power-saving.conf 	/etc/modprobe.d/
 #copy_file hang-on-shutdown.conf  	/etc/modprobe.d/
 #copy_file 98qtconfig /etc/X11/Xsession.d/
-#copy_file compton.desktop /etc/skel/.local/share/applications/
-#copy_file compton-conf.desktop /etc/skel/.local/share/applications/
 #copy_file ndisgtk.desktop /usr/share/applications/
 #copy_file ndisgtk.svg /usr/share/icons/hicolor/scalable/apps/
 #copy_file ndisgtk.svg /usr/share/icons/hicolor/48x48/apps/
-copy_file uswsusp.conf /etc/
-copy_file plymouthd.conf /etc/plymouth/		--create
+#copy_file plymouthd.conf /etc/plymouth/ --create
 #copy_file luckybackup.desktop /usr/share/applications/
 #copy_file luckybackup-su.desktop /usr/share/applications/
 #copy_file luckybackup.svg /usr/share/icons/hicolor/scalable/apps/
@@ -53,15 +50,15 @@ copy_file plymouthd.conf /etc/plymouth/		--create
 # copy custom Workbench stuff
 copy_file workbench-tools.desktop /usr/share/applications/
 copy_file workbench-tools.desktop /etc/skel/.config/autostart/
-copy_dir desktop_files/ /usr/share/applications/workbench/	--create
+copy_dir desktop_files/ /usr/share/applications/workbench/ --create
 copy_file workbench-tools.list /etc/custom-toolbox/
-copy_file wood-architect-table-work.jpeg /usr/share/backgrounds/MXLinux/	--create
-copy_file wood-architect-table-work-BW.jpeg /usr/share/backgrounds/MXLinux/lightdm/	--create
+copy_file wood-architect-table-work.jpeg /usr/share/backgrounds/MXLinux/ --create
+copy_file wood-architect-table-work-BW.jpeg /usr/share/backgrounds/MXLinux/lightdm/ --create
 copy_file lightdm-gtk-greeter.conf /etc/lightdm/
 copy_file workbench-sudoers /etc/sudoers.d/
 copy_dir defaults/panel/ /etc/skel/.config/xfce4/panel/	--create
 copy_dir defaults/xfconf/xfce-perchannel-xml/ /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/	--create
-copy_dir defaults/qt4-fsarchiver/ /etc/skel/.config/qt4-fsarchiver/	--create
+copy_dir defaults/qt4-fsarchiver/ /etc/skel/.config/qt4-fsarchiver/ --create
 copy_file mx-snapshot.conf /etc/
 ln -sf /usr/games/blockout2 /usr/bin/blockout2 # add a link in $PATH
 
