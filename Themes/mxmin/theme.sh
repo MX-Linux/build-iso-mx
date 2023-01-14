5c629fc89a87d8321297cccd5768a639f758f610
Adrian <adrian@mxlinux.org>
2023-01-14 15:43:17 -0500

    MIN: update for the release

--- Themes/mxmin/theme.sh
+++ Themes/mxmin/theme.sh
@@ -4,28 +4,23 @@ THEME_DIR=$(dirname $(readlink -f $0))
 source $THEME_DIR/../theme-functions.sh
 start_theme "$@"
 
-rm -R /etc/skel/.conky
-mkdir -p /etc/skel/.local/share/applications/
 copy_file icons.screen.latest.rc /etc/skel/.config/xfce4/desktop/
-copy_file grub                  /etc/default/
+copy_file grub                   /etc/default/
 copy_file 55-tweak-override.conf /etc/polkit-1/localauthority.conf.d/
 copy_file 10_linux              /etc/grub.d/
 copy_file 30_os-prober			/etc/grub.d/
 copy_file 30_uefi-firmware		/etc/grub.d/
-copy_dir desktop-base/               /usr/share/desktop-base/          --create
+copy_dir desktop-base/          /usr/share/desktop-base/          --create
 copy_file rc.local              /etc/
 copy_file libuser.conf          /etc/
 copy_file modules               /etc/
-copy_file timezone		/etc/
-copy_file nano.desktop		/usr/share/applications/
-copy_file lightdm.conf		/etc/lightdm/
+copy_file timezone		        /etc/
+copy_file lightdm.conf		    /etc/lightdm/
 copy_file lightdm-gtk-greeter.conf /etc/lightdm/
-copy_file pc-speaker.conf	/etc/modprobe.d/
+copy_file pc-speaker.conf	    /etc/modprobe.d/
 copy_file desktop.data          /usr/local/share/boot-menus/
 copy_file desktop.menu          /usr/local/share/boot-menus/
 copy_file 20-thinkpad.conf      /usr/share/X11/xorg.conf.d/
-copy_file pppoeconf.desktop     /usr/share/applications/
-copy_file honesty-muted.jpg     /usr/share/backgrounds/
-copy_file Honesty.jpg		/usr/share/backgrounds/
+copy_file MX_Tangram.png        /usr/share/backgrounds/
 
 exit
