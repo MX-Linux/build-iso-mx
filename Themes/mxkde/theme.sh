61da5755a1aa325d5bef4fbdaa20c585663441a0
Dolphin Oracle <dolphinoracle@gmail.com>
2023-05-18 21:13:59 -0400

    enable ufw by default

--- Themes/mxkde/theme.sh
+++ Themes/mxkde/theme.sh
@@ -29,6 +29,7 @@ copy_file 20-thinkpad.conf      /usr/share/X11/xorg.conf.d/
 #copy_file 98qtconfig /etc/X11/Xsession.d/
 copy_file yad-icon-browser.desktop /etc/skel/.local/share/applications/ --create
 copy_file plymouthd.conf /etc/plymouth/ --create
+copy_file ufw.conf       /etc/ufw/ --create
 
 ##alpha
 
