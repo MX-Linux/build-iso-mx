7d1340c4677c04d6c54fe6dad1f37ea12cfa1418
Adrian <adrian@mxlinux.org>
2025-04-12 19:40:40 -0400

    Workbench: fix wallpaper

--- Themes/mx-workbench/theme.sh
+++ Themes/mx-workbench/theme.sh
@@ -4,7 +4,6 @@ THEME_DIR=$(dirname $(readlink -f $0))
 source $THEME_DIR/../theme-functions.sh
 start_theme "$@"
 
-mkdir -p /etc/skel/.local/share/applications/
 copy_file grub                  /etc/default/
 copy_file 55-tweak-override.conf /etc/polkit-1/localauthority.conf.d/
 copy_dir extra/               	/usr/share/fonts/extra       	--create
@@ -28,7 +27,7 @@ copy_file gitstatusd-linux-x86_64 /etc/skel/.cache/gitstatus/ --create
 copy_file adduser.conf /etc/
 
 # copy custom Workbench stuff
-copy_file workbench-tools.desktop /usr/share/applications/
+copy_file workbench-tools.desktop /usr/share/applications/ --create
 copy_file workbench-tools.desktop /etc/skel/.config/autostart/
 copy_dir desktop_files/ /usr/share/applications/workbench/ --create
 copy_file workbench-tools.list /etc/custom-toolbox/
@@ -41,6 +40,7 @@ copy_dir defaults/xfconf/xfce-perchannel-xml/ /etc/skel/.config/xfce4/xfconf/xfc
 copy_dir defaults/terminal/ /etc/skel/.config/xfce4/terminal/ --create
 copy_file mx-snapshot.conf /etc/
 copy_file custom-toolbox.conf /etc/custom-toolbox/
+copy_file default23.png /usr/share/backgrounds/
 ln -sf /usr/games/blockout2 /usr/bin/blockout2 # add a link in $PATH
 
 exit
