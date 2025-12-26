77a53ded51c3e0c256884942e9b2b0186bdf1c6b
Adrian <adrian@mxlinux.org>
2025-12-26 16:50:46 -0500

    Arch: theme cleanup

--- Themes/archkde/theme.sh
+++ Themes/archkde/theme.sh
@@ -8,17 +8,9 @@ start_theme "$@"
 # archkde: Arch-friendly subset of the mxkde theme
 # (avoid Debian/MX-specific files like /etc/default/grub, /etc/modules, etc).
 #
-
-mkdir -p /etc/skel/.local/share/applications/
-copy_file conky.desktop		/etc/skel/.config/autostart/	--create
 copy_dir desktop-base/		/usr/share/desktop-base/	--create
-# copy_dir extra/			/usr/share/fonts/extra		--create
 copy_file rc.local		/etc/
-copy_file timezone		/etc/
-copy_file sddm.conf		/etc/
 copy_file pc-speaker.conf	/etc/modprobe.d/
-# copy_file 20-thinkpad.conf	/usr/share/X11/xorg.conf.d/
-copy_file yad-icon-browser.desktop /etc/skel/.local/share/applications/	--create
 copy_file plymouthd.conf	/etc/plymouth/	--create
 copy_file ufw.conf		/etc/ufw/	--create
 copy_file zramswap.service	/etc/systemd/system/
