6abb7cd3bfa68956623c1d960747325750a9a879
Adrian <adrian@mxlinux.org>
2025-12-26 16:50:46 -0500

    Arch: theme cleanup

--- Themes/archxfce/theme.sh
+++ Themes/archxfce/theme.sh
@@ -19,14 +19,11 @@ copy_file lightdm-gtk-greeter.conf /etc/lightdm/ --create
 
 # Hardware niceties.
 copy_file pc-speaker.conf  /etc/modprobe.d/            --create
-copy_file 20-thinkpad.conf /usr/share/X11/xorg.conf.d/ --create
 
 # Optional components: safe even if the corresponding packages aren't installed.
 copy_file plymouthd.conf /etc/plymouth/ --create
 
 # Default live-user autostarts and Xfce settings.
-mkdir -p /etc/skel/.local/share/applications/ /etc/skel/.config/autostart/
-copy_file conky.desktop             /etc/skel/.config/autostart/ --create
 copy_file magnus-autostart.desktop  /etc/skel/.config/autostart/ --create
 copy_file xfce4-desktop.xml /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/ --create
 
@@ -34,4 +31,3 @@ copy_file xfce4-desktop.xml /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/ 
 systemctl enable systemd-resolved
 
 exit 0
-
