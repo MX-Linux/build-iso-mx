e7ca51dcfe8f06774147d3e3225a4a666e4161e6
Adrian <adrian@mxlinux.org>
2025-12-27 00:31:10 -0500

    Cache sudo credentials if needed

--- Themes/archkde/theme.sh
+++ Themes/archkde/theme.sh
@@ -9,10 +9,11 @@ start_theme "$@"
 # (avoid Debian/MX-specific files like /etc/default/grub, /etc/modules, etc).
 #
 copy_dir desktop-base/		/usr/share/desktop-base/	--create
+copy_dir extra/			/usr/share/fonts/extra		--create
 copy_file rc.local		/etc/
 copy_file pc-speaker.conf	/etc/modprobe.d/
-copy_file plymouthd.conf	/etc/plymouth/	--create
-copy_file ufw.conf		/etc/ufw/	--create
+copy_file plymouthd.conf	/etc/plymouth/			--create
+copy_file ufw.conf		/etc/ufw/			--create
 copy_file zramswap.service	/etc/systemd/system/
 
 # Enable services
