8934227dd98e7ea5908a30925c760d6c7e7d2d96
Adrian <adrian@mxlinux.org>
2026-01-10 21:16:12 -0500

    Restore theme from mx25

--- Themes/mx/theme.sh
+++ Themes/mx/theme.sh
@@ -4,21 +4,25 @@ THEME_DIR=$(dirname $(readlink -f $0))
 source $THEME_DIR/../theme-functions.sh
 start_theme "$@"
 
-copy_file grub			/etc/default/
-copy_dir desktop-base/		/usr/share/desktop-base/	--create
-copy_dir extra/			/usr/share/fonts/extra		--create
-copy_file rc.local		/etc/
-copy_file libuser.conf		/etc/
-copy_file modules		/etc/
+mkdir -p /etc/skel/.local/share/applications/
+copy_file conky.desktop  	/etc/skel/.config/autostart/
+copy_file grub                  /etc/default/
+copy_dir desktop-base/          /usr/share/desktop-base/	--create
+copy_dir extra/               	/usr/share/fonts/extra       	--create
+copy_file rc.local              /etc/
+copy_file libuser.conf          /etc/
+copy_file modules               /etc/
+copy_file timezone		/etc/
 copy_file lightdm.conf		/etc/lightdm/
 copy_file lightdm-gtk-greeter.conf /etc/lightdm/
 copy_file pc-speaker.conf	/etc/modprobe.d/
-copy_file desktop.data		/usr/share/boot-menus/
-copy_file desktop.menu		/usr/share/boot-menus/
-copy_file plymouthd.conf	/etc/plymouth/	--create
-copy_file ufw.conf		/etc/ufw/	--create
+copy_file desktop.data          /usr/share/boot-menus/
+copy_file desktop.menu          /usr/share/boot-menus/
+copy_file 20-thinkpad.conf      /usr/share/X11/xorg.conf.d/
+copy_file plymouthd.conf 	/etc/plymouth/	--create
+copy_file ufw.conf       	/etc/ufw/ 	--create
 copy_file magnus-autostart.desktop /etc/skel/.config/autostart
-copy_file zramswap.service	/etc/systemd/system/
+copy_file zramswap.service 	/etc/systemd/system/
 
 ##alpha
 
