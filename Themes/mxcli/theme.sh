de8c998201ef25ecb90da727a0b696d3ae5fa680
Adrian <adrian@mxlinux.org>
2026-01-19 12:10:41 -0500

    Disable services that we don't install

--- Themes/mxcli/theme.sh
+++ Themes/mxcli/theme.sh
@@ -14,4 +14,8 @@ copy_file pc-speaker.conf	/etc/modprobe.d/
 copy_file desktop.data          /usr/share/boot-menus/
 copy_file desktop.menu          /usr/share/boot-menus/
 
+# Disable services that require executables from mx-tweak (not installed in CLI)
+systemctl disable live-broadcomstartup.service 2>/dev/null || true
+systemctl disable live-clock12or24.service 2>/dev/null || true
+
 exit
